c1d498f088a7d14d013c54f89defffdf3e2853d4
JeanPiton <jean.orpiton@gmail.com>
2023-05-17 13:54:44 -0300

    App mockup

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,14 @@ export default function App() {
     <APP>
       <LOGO>
         <img src={logo}/>
-        ZapRecall
+        <>ZapRecall</>
       </LOGO>
+      <QUESTIONS>
+
+      </QUESTIONS>
+      <BOTTOM>
+        0/4 CONCLUÍDOS
+      </BOTTOM>
     </APP>
   )
 }
@@ -22,13 +28,34 @@ const APP = styled.div`
 `;
 
 const LOGO = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: Righteous;
   font-weight: 400;
   font-size: 36px;
   color: white;
+  margin: 48px;
 
   & > img{
     width: 52px;
     height: 60px;
   }
+`;
+
+const QUESTIONS = styled.div``;
+
+const BOTTOM = styled.div`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  font-family: Recursive;
+  font-weight: 400;
+  font-size: 18px;
 `;
